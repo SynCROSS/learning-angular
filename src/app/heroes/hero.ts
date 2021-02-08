0d5fd8adc3a50441b9a909368f433d0bf52d852b
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-02-08 23:11:04 +0900

    Heroes List

---
+++ src/app/heroes/hero.ts
@@ -0,0 +1,11 @@
+export interface Hero {
+  id: number;
+  name: string;
+  element_type: string;
+  level: number;
+  type: string[];
+  effect?: string;
+  text?: string;
+  atk: number;
+  def: number;
+}
